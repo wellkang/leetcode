25343ae1a5eda4df2ccf1561f12dd7204fe4b63d
“wangliang <wl_workjob@163.com>
2016-07-28 18:25:34 +0800

    combination_sum four

---
+++ combination_sum.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+"""
+
+
+def combination_sum4(nums, target):
+    dp = [0 for i in range(target + 1)]
+    dp[0] = 1
+    for i in range(target + 1):
+        if dp[i] == 0:
+            continue
+        for n in nums:
+            if i + n <= target:
+                dp[i+n] += dp[i]
+    return dp[-1]
+
+
+if __name__ == '__main__':
+    print combination_sum4([1, 2, 3, 4], 10)
